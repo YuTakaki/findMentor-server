5433c8cdc3eed897e4b8951eb47fa44a9183cbd9
yutakaki <yu.takaki2000@gmail.com>
2022-03-09 20:47:21 +0900

    added serializer and a viewset in account app

--- account/views.py
+++ account/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import User
+from .serializers import RegisterSerializer
 
 # Create your views here.
+class RegisterView(generics.GenericAPIView):
+  serializer_class = RegisterSerializer
+  queryset = User.objects.none()
+
+  def post(self, request):
+    print(request.data)
+    return True
