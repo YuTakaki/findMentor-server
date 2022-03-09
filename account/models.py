3a1cd85a6cb8d0a14134f48e6b922dd7b46351bf
yutakaki <yu.takaki2000@gmail.com>
2022-03-09 20:54:34 +0900

    removed migrations and change id type to uuid in account

--- account/models.py
+++ account/models.py
@@ -1,8 +1,10 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, max_length=32)
   username = models.CharField(max_length=100, unique=True)
   email = models.CharField(max_length=100, unique=True)
   password = models.CharField(max_length=10000)
