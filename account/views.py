661cb159598f11b752f82e2f0dc5fdf94156f9df
yutakaki <yu.takaki2000@gmail.com>
2022-03-26 22:34:11 +0900

    change userInformationView post request

--- account/views.py
+++ account/views.py
@@ -31,6 +31,30 @@ class RegisterView(generics.GenericAPIView):
     res.set_cookie( key="token", value=str(token.access_token), httponly=True)
     return res
 
+
+def check(user, token=None):
+  if user.data.get('account_type') == 'mentor':
+      skills = Skills.objects.filter(user=user.data.get('id'))
+      error = None
+      if user.data.get('job_position') is None:
+        error = 0
+      elif len(skills) == 0:
+        error = 1
+      elif user.data.get('pay_rate') is None:
+        error = 2
+      data = user.data
+      return {
+        'user': data,
+        'error': error,
+        'token': token
+      }
+  data = user.data
+  return {
+    'user': data,
+    'token': token
+  }
+
+
 class LoginView(generics.GenericAPIView):
   serializer_class = LoginSerializer
   queryset = User.objects.none()
@@ -57,52 +81,44 @@ class LoginView(generics.GenericAPIView):
     
     token = RefreshToken.for_user(user)
     serialize = RegisterSerializer(user)
+    data = check(serialize)
 
-    res = Response({
-      'user': serialize.data,
-      'token': str(token.access_token)
-    })
+    res = Response(data)
 
     res.set_cookie( key="token", value=str(token.access_token), httponly=True)
     return res
+
+
 
 class VerifyTokenView(generics.GenericAPIView):
   serializer_class = RegisterSerializer
 
   def get(self, request):
     serialize_user = self.get_serializer(request.user)
-    if serialize_user.data.get('account_type') == 'mentor':
-      skills = Skills.objects.filter(user=serialize_user.data.get('id'))
-      error = None
-      if serialize_user.data.get('job_position') is None:
-        error = 0
-      elif len(skills) == 0:
-        error = 1
-      elif serialize_user.data.get('pay_rate') is None:
-        error = 2
-      data = serialize_user.data
-      return Response({
-        'user': data,
-        'error': error
-      })
-    data = serialize_user.data
-    return Response({
-      'user': data
-    })
+    data = check(serialize_user)
+    return Response(data)
+    
 
 class UserInformationView(generics.GenericAPIView):
   serializer_class = UserInformationSerializer
   def post(self, request):
     # user = request.user
     user = request.user
-    user.job_position = request.data.get('job_position')
-    profile_img = request.data.get('profile_img')
-    if not isinstance(profile_img, str):
-      user.profile_img = profile_img
-    user.bio = request.data.get('bio')
+    pay_rate = request.data.get('pay_rate')
+    if pay_rate is not None:
+      user.pay_rate = pay_rate
+      error = 2
+    else:
+      user.job_position = request.data.get('job_position')
+      profile_img = request.data.get('profile_img')
+      if not isinstance(profile_img, str):
+        user.profile_img = profile_img
+      user.bio = request.data.get('bio')
+      error = None
     user.save()
     serializer = RegisterSerializer(user)
 
     return Response({
-      'user' : serializer.data
+      'user' : serializer.data,
+      'error' : error
     })
