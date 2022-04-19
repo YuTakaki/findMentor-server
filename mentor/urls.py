8287729f3ad51d8d2d44ad5f005690450a0d6cfb
yutakaki <yu.takaki2000@gmail.com>
2022-04-19 22:15:16 +0900

    add filter view

--- mentor/urls.py
+++ mentor/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.MentorView.as_view()),
-    
+  path('filter', views.filterMentorView)
 ]
--- mentor/views.py
+++ mentor/views.py
@@ -1,8 +1,10 @@
+from math import inf
 from django.shortcuts import render
 from rest_framework import generics
 from account.models import User
 from .serializers import MentorSerializer
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
 class MentorView(generics.GenericAPIView):
   serializer_class = MentorSerializer
@@ -14,3 +16,22 @@ class MentorView(generics.GenericAPIView):
       'mentors': serialize.data
     })
     
+@api_view(['GET'])
+def filterMentorView(request):
+  skills = request.query_params.get('skills').split(',')
+  min = request.query_params.get('min') 
+  max = request.query_params.get('max')
+  if skills[0] != '':
+    mentors = User.objects.filter(account_type='mentor', skills__skill__in = skills)
+  else:
+    mentors = User.objects.filter(account_type='mentor')
+  if min != '' and max != '':
+    mentors = mentors.filter(pay_rate__gte = min, pay_rate__lte= max)
+  elif min == '' and max != '':
+    mentors = mentors.filter(pay_rate__lte= max)
+  elif min != '' and max == '':
+    mentors = mentors.filter(pay_rate__gte= min)
+  serialize = MentorSerializer(mentors.distinct(), many=True)
+  return Response({
+    'mentors': serialize.data
+  })
