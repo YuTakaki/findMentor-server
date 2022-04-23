09ceef77a510fb26ca1de665f1a390bf6243d61c
yutakaki <yu.takaki2000@gmail.com>
2022-04-24 00:38:56 +0900

    reposition url in mentor

--- mentor/urls.py
+++ mentor/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.MentorView.as_view()),
-  path('<str:pk>', views.getMentorView),
   path('filter', views.filterMentorView),
+  path('<str:pk>', views.getMentorView),
 ]
